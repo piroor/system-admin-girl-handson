455d99fa68482e4a2415bab3d1b3aefa34bbfa4c
Piro / YUKI Hiroshi <piro.outsider.reflex@gmail.com>
2015-04-08 23:31:10 +0900

    Backup old configuration with timestamp

--- script/setup-back.sh
+++ script/setup-back.sh
@@ -7,14 +7,15 @@ echo 'Setting up this computer as the "back"...'
 ETH0_MAC_ADDRESS=$(ifconfig eth0 | grep HWaddr | sed -r -e 's/^.* ([0-9A-Z:]+)/\1/')
 ETH1_MAC_ADDRESS=$(ifconfig eth1 | grep HWaddr | sed -r -e 's/^.* ([0-9A-Z:]+)/\1/')
 ETH0_CONFIG=/etc/sysconfig/network-scripts/ifcfg-eth0
+ETH0_CONFIG_BACKUP=~/ifcfg-eth0.bak.$(date +%Y-%m-%d_%H-%M-%S)
 ETH1_CONFIG=/etc/sysconfig/network-scripts/ifcfg-eth1
 
 echo "Detected MAC Address of eth0: $ETH0_MAC_ADDRESS"
 echo "Detected MAC Address of eth1: $ETH1_MAC_ADDRESS"
 
 echo 'Deactivating eth0...'
-mv $ETH0_CONFIG ~/ifcfg-eth0.bak
-cat ~/ifcfg-eth0.bak | sed -r -e 's/ONBOOT="yes"/ONBOOT="no"/' > $ETH0_CONFIG
+mv $ETH0_CONFIG $ETH0_CONFIG_BACKUP
+cat $ETH0_CONFIG_BACKUP | sed -r -e 's/ONBOOT="yes"/ONBOOT="no"/' > $ETH0_CONFIG
 
 echo 'Activating eth1...'
 echo 'DEVICE="eth1"'                >  $ETH1_CONFIG
