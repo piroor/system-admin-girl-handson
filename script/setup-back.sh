93ca193bd71d8c64e492f979f5ce00954b6d1f82
Piro / YUKI Hiroshi <piro.outsider.reflex@gmail.com>
2015-04-09 01:26:23 +0900

    Align

--- script/setup-back.sh
+++ script/setup-back.sh
@@ -12,14 +12,14 @@ ETH1_CONFIG=/etc/sysconfig/network-scripts/ifcfg-eth1
 echo "Detected MAC Address of eth1: $ETH1_MAC_ADDRESS"
 
 echo 'Activating eth1...'
-echo 'DEVICE="eth1"'                >  $ETH1_CONFIG
-echo "HWADDR=\"$ETH1_MAC_ADDRESS\"" >> $ETH1_CONFIG
-echo 'BOOTPROTO="static"'           >> $ETH1_CONFIG
+echo 'DEVICE="eth1"'                 >  $ETH1_CONFIG
+echo "HWADDR=\"$ETH1_MAC_ADDRESS\""  >> $ETH1_CONFIG
+echo 'BOOTPROTO="static"'            >> $ETH1_CONFIG
 echo "IPADDR=\"$STATIC_IP_ADDRESS\"" >> $ETH1_CONFIG
-echo 'NETMASK="255.255.255.0"'      >> $ETH1_CONFIG
-echo 'NM_CONTROLLED="no"'           >> $ETH1_CONFIG
-echo 'TYPE="Ethernet"'              >> $ETH1_CONFIG
-echo 'ONBOOT="yes"'                 >> $ETH1_CONFIG
+echo 'NETMASK="255.255.255.0"'       >> $ETH1_CONFIG
+echo 'NM_CONTROLLED="no"'            >> $ETH1_CONFIG
+echo 'TYPE="Ethernet"'               >> $ETH1_CONFIG
+echo 'ONBOOT="yes"'                  >> $ETH1_CONFIG
 
 echo 'Restarting interfaces...'
 service network restart
