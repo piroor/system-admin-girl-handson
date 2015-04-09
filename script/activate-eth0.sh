19441244de5563cbca9e2abc45e5ce72924cc440
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2015-04-09 16:04:05 +0900

    Add comments for description

--- script/activate-eth0.sh
+++ script/activate-eth0.sh
@@ -4,6 +4,9 @@
 
 
 echo 'Activating eth0...'
+# インターネットに接続されているインターフェースであるeth0を有効化する。
+# これにより、このコンピュータはインターネットからもアクセス可能になる。
+# See also: https://www.conoha.jp/blog/tech/4451.html
 
 ETH0_CONFIG=/etc/sysconfig/network-scripts/ifcfg-eth0
 ETH0_CONFIG_BACKUP=~/ifcfg-eth0.bak.$(date +%Y-%m-%d_%H-%M-%S)
