cbdc73b52f991657c21c5a8974a9fc161566ab2e
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2015-04-09 16:01:29 +0900

    Add comments for description

--- script/disconnect-back.sh
+++ script/disconnect-back.sh
@@ -6,10 +6,9 @@ echo 'Setting up this computer as the "back"...'
 
 
 echo 'Deactivating eth0...'
-
-ETH0_MAC_ADDRESS=$(ifconfig eth0 | grep HWaddr | sed -r -e 's/^.* ([0-9A-Z:]+)/\1/')
-
-echo "Detected MAC Address of eth0: $ETH0_MAC_ADDRESS"
+# インターネットに接続されているインターフェースであるeth0を無効化する。
+# これにより、このコンピュータは完全にローカルネットワーク内専用になる。
+# See also: https://www.conoha.jp/blog/tech/4451.html
 
 ETH0_CONFIG=/etc/sysconfig/network-scripts/ifcfg-eth0
 ETH0_CONFIG_BACKUP=~/ifcfg-eth0.bak.$(date +%Y-%m-%d_%H-%M-%S)
